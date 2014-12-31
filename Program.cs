accd35277f843f0cc340326989add210e35f0ec3
Matt Ward <ward.matt@gmail.com>
2014-12-31 11:08:52 +0000

    Add sample project that tests V8.NET.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+using V8.Net;
+
+namespace V8Test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var program = new Program();
+			program.Run();
+			
+			Console.Write("Press any key to continue...");
+			Console.ReadKey(true);
+		}
+		
+		void Run()
+		{
+			using (var engine = new V8Engine()) {
+				
+				// Return value from JavaScript.
+				using (Handle result = engine.Execute("var i = 10; i;", "V8.NET", true)) {
+					Console.WriteLine(result.AsString);
+				}
+				
+				engine.RegisterType(typeof(SystemConsole), null, true, ScriptMemberSecurity.Locked);
+				engine.GlobalObject.SetProperty("console", new SystemConsole());
+				using (Handle handle = engine.Execute("console.Print('foo');", "V8.NET", true)) {
+					
+				}
+			}
+		}
+	}
+}
---
+++ SystemConsole.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+
+using System;
+
+namespace V8Test
+{
+	public class SystemConsole
+	{
+		public SystemConsole() { }
+
+		public void Print(string iString)
+		{
+			Console.WriteLine(iString);
+		}
+	}
+}
